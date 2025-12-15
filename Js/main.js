2a0139feb1cfeb9e2a09cd399e965eeb00dff680
bardahan96 <bardahan22@icloud.com>
2025-12-15 16:47:06 +0200

    js: function : clsoe modal | cstom select | modal handelers
    ui: breakpoints till 1024px

---
+++ Js/main.js
@@ -0,0 +1,159 @@
+const modal = document.querySelector('.signup-modal');
+const signUpBtn = document.getElementById("signUp-openBtn");
+const closeModalBtn = modal.querySelector('.close-btn');
+const continueBtnModal = modal.querySelector('button[data-action="next"]');
+const addMorePlatforms = document.getElementById("addMorePlatforms");
+const modalBottom = modal.querySelector('.modal-bottom');
+const overflowPlatform = modal.querySelector('.signup-step[data-step="2"]')
+
+
+
+function addMoreSocialPlatform () {
+    const socialPlatform = modal.querySelector(".social-platform");
+    if(!socialPlatform) return;
+
+    const newPlatform = socialPlatform.cloneNode(true);
+
+
+  const hiddenInput = newPlatform.querySelector('.social-platform-store');
+  if (hiddenInput) hiddenInput.value = '';
+
+
+    // clean platforms
+    newPlatform.querySelector('input').value = ''
+    newPlatform.querySelector('.platform-name').textContent = 'Platform'
+
+    const deletePlatform = document.createElement("div");
+    deletePlatform.classList.add('delete-platform')
+    deletePlatform.innerHTML = `
+    <img src="./Assets/icons/garbage-icon.svg" alt="garbage-arrow">
+    `
+
+    newPlatform.appendChild(deletePlatform)
+    
+    
+    addMorePlatforms.before(newPlatform)
+
+    handleOverflow(modalBottom, overflowPlatform)
+
+    function deleteNewPlatform(e) {
+        const platform = e.currentTarget.closest('.social-platform');
+        if (!platform) return;
+      
+        handleOverflow(modalBottom, overflowPlatform)
+        platform.remove();
+      }
+
+      
+
+    deletePlatform.addEventListener("click", deleteNewPlatform)
+
+}
+
+function closeModal () {
+    modal.close()
+
+    modal.querySelectorAll('.signup-step')
+    .forEach(step => step.classList.remove('is-visible'));
+
+    modal.querySelectorAll('.custom-select.is-open')
+    .forEach(select => select.classList.remove('is-open'));
+
+    modal.style.setProperty('--progressBar', '0%');
+}
+
+function openModal () {
+    const firstStep = modal.querySelector('[data-step="1"]')
+    firstStep.classList.add("is-visible")
+    modal.showModal()
+
+    updateProgressBar(1 , modal.querySelectorAll('[data-step]').length);
+
+}
+
+function goToNextStep () {
+    const steps = modal.querySelectorAll("[data-step]");
+    const stepsArrays = Array.from(steps, step => 
+        Number(step.dataset.step)
+    );
+
+    let currentStep = modal.querySelector('[data-step].is-visible');
+    if(!currentStep) return;
+
+    let currentStepNumber = Number(currentStep.dataset.step);
+    let nextStepNumber = currentStepNumber + 1
+
+    
+
+    if(nextStepNumber <= stepsArrays.length){
+        currentStep.classList.remove('is-visible');
+        
+        let nextStep = modal.querySelector(`[data-step="${nextStepNumber}"]`);
+        if (!nextStep) return;
+        nextStep.classList.add("is-visible");
+        nextStep.removeAttribute('hidden')
+
+        updateProgressBar(nextStepNumber, stepsArrays.length)
+    }
+
+
+
+}
+
+function updateProgressBar (currentStep, totalSteps) {
+    let percentBar = ( currentStep / totalSteps) * 100;
+    modal.style.setProperty('--progressBar', `${percentBar}%`)
+}
+
+function handleCustomSelect(e) {
+
+    const opener = e.target.closest('.custom-select-opener');
+    if (opener) {
+      const select = opener.closest('.custom-select');
+
+  
+      select.classList.toggle('is-open');
+      return;
+    }
+  
+    const option = e.target.closest('.custom-select-options li');
+    if (option) {
+      const select = option.closest('.custom-select');
+      const input = select.querySelector('.social-platform-store');
+      const label = select.querySelector('.platform-name');
+      const value = option.dataset.value;
+  
+      input.value = value;
+      label.textContent = value;
+      select.classList.remove('is-open');
+    }
+}
+
+function handleOverflow(fixedElement, overflowEl) {
+    if (!fixedElement || !overflowEl) return;
+  
+    const hasOverflow =
+      overflowEl.scrollHeight > overflowEl.clientHeight;
+
+          fixedElement.classList.toggle('has-shadow', hasOverflow);
+  
+  }
+  
+modal.addEventListener('click', handleCustomSelect);
+
+signUpBtn.addEventListener("click" , openModal)
+
+closeModalBtn.addEventListener("click" , closeModal)
+
+continueBtnModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    goToNextStep()
+})
+
+addMorePlatforms.addEventListener("click" , (e) => {
+    e.preventDefault();
+    addMoreSocialPlatform();
+})
+
+
+
